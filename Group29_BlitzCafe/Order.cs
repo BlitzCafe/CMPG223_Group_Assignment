b9c545af7a92bf983003417919f8f22a5e541781
Sc Van Wyk <44749082@mynwu.ac.za>
2024-08-09 11:41:06 +0200

    Added order and menuitem classes

---
+++ Group29_BlitzCafe/Order.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group29_BlitzCafe
+{
+    class Order
+    {
+        //create attributes, orderID can only be set once
+        private readonly int orderID;
+        private DateTime orderDate;
+        private bool isPayed;
+        private bool loyaltyPointsUsed;
+
+        //Create new order object
+        public Order(int orderID, DateTime orderDate, bool isPayed, bool loyaltyPointsUsed)
+        {
+            this.orderID = orderID;
+            setOrderDate(orderDate);
+            setIsPayed(isPayed);
+            setLoyaltyPointsUsed(loyaltyPointsUsed);
+        }
+        //set order date
+        public void setOrderDate (DateTime orderDate)
+        {
+            this.orderDate = orderDate;
+        }
+        //set wether the order has been payed
+        public void setIsPayed( bool isPayed)
+        {
+            this.isPayed = isPayed;
+        }
+        //set if loyalty points were used 
+        public void setLoyaltyPointsUsed(bool loyaltyPointsUsed)
+        {
+            this.loyaltyPointsUsed = loyaltyPointsUsed;
+        }
+        //get Order ID
+        public int getOrderID()
+        {
+            return this.orderID;
+        }
+        //get date ordered
+        public DateTime getOrderDate()
+        {
+            return this.orderDate;
+        }
+        //get payment status
+        public bool getIsPayed()
+        {
+            return this.isPayed;
+        }
+        //get if loyalty points were used
+        public bool getLoyaltyPointsUsed()
+        {
+            return this.loyaltyPointsUsed;
+        }
+
+        //overwrite to string method
+        public string toString()
+        {
+            return "Order ID: " + getOrderID() + "\tOrder Date: " + getOrderDate() 
+                + "\tIs Payed: " + getIsPayed() + "\tLoyalty Points Used:" + getLoyaltyPointsUsed();
+        }
+    }
+}
