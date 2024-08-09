0dd9afdb6991848e2b3ea71615f261da0a795ce4
Juane Schoeman <138971307+JuaneSchoeman@users.noreply.github.com>
2024-08-09 14:35:21 +0200

    Added using

--- Group29_BlitzCafe/CustomerPage.cs
+++ Group29_BlitzCafe/CustomerPage.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Windows.Forms;
 
 namespace Group29_BlitzCafe
