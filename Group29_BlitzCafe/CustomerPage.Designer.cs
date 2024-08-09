a73878a0317529dce5015518d2a472e8e4556c96
Juane Schoeman <138971307+JuaneSchoeman@users.noreply.github.com>
2024-08-09 14:04:10 +0200

    Added DBGrid functionality

--- Group29_BlitzCafe/CustomerPage.Designer.cs
+++ Group29_BlitzCafe/CustomerPage.Designer.cs
@@ -30,11 +30,10 @@ namespace Group29_BlitzCafe
         private void InitializeComponent()
         {
             this.btnAddNew = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dbgCustomerInfo = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.txtDate = new System.Windows.Forms.TextBox();
             this.lblDateJoined = new System.Windows.Forms.Label();
-            this.cbxLoyaltyMem = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtCellNo = new System.Windows.Forms.TextBox();
             this.txtLName = new System.Windows.Forms.TextBox();
@@ -44,7 +43,7 @@ namespace Group29_BlitzCafe
             this.lblFirstName = new System.Windows.Forms.Label();
             this.lblCustomerID = new System.Windows.Forms.Label();
             this.btnConfirmUpdate = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgCustomerInfo)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -59,22 +58,21 @@ namespace Group29_BlitzCafe
             this.btnAddNew.UseVisualStyleBackColor = true;
             this.btnAddNew.Click += new System.EventHandler(this.btnAddNew_Click);
             // 
-            // dataGridView1
+            // dbgCustomerInfo
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(35, 106);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(438, 346);
-            this.dataGridView1.TabIndex = 1;
+            this.dbgCustomerInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbgCustomerInfo.Location = new System.Drawing.Point(35, 106);
+            this.dbgCustomerInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dbgCustomerInfo.Name = "dbgCustomerInfo";
+            this.dbgCustomerInfo.RowHeadersWidth = 62;
+            this.dbgCustomerInfo.RowTemplate.Height = 28;
+            this.dbgCustomerInfo.Size = new System.Drawing.Size(438, 346);
+            this.dbgCustomerInfo.TabIndex = 1;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.txtDate);
             this.groupBox1.Controls.Add(this.lblDateJoined);
-            this.groupBox1.Controls.Add(this.cbxLoyaltyMem);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txtCellNo);
             this.groupBox1.Controls.Add(this.txtLName);
@@ -108,16 +106,6 @@ namespace Group29_BlitzCafe
             this.lblDateJoined.Size = new System.Drawing.Size(92, 17);
             this.lblDateJoined.TabIndex = 10;
             this.lblDateJoined.Text = "Date Joined: ";
-            // 
-            // cbxLoyaltyMem
-            // 
-            this.cbxLoyaltyMem.AutoSize = true;
-            this.cbxLoyaltyMem.Location = new System.Drawing.Point(126, 280);
-            this.cbxLoyaltyMem.Name = "cbxLoyaltyMem";
-            this.cbxLoyaltyMem.Size = new System.Drawing.Size(152, 21);
-            this.cbxLoyaltyMem.TabIndex = 9;
-            this.cbxLoyaltyMem.Text = "Is Loyalty Member?";
-            this.cbxLoyaltyMem.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -192,6 +180,7 @@ namespace Group29_BlitzCafe
             this.btnConfirmUpdate.TabIndex = 0;
             this.btnConfirmUpdate.Text = "Confirm Changes";
             this.btnConfirmUpdate.UseVisualStyleBackColor = true;
+            this.btnConfirmUpdate.Click += new System.EventHandler(this.btnConfirmUpdate_Click);
             // 
             // CustomerPage
             // 
@@ -199,13 +188,13 @@ namespace Group29_BlitzCafe
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(942, 517);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dbgCustomerInfo);
             this.Controls.Add(this.btnAddNew);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "CustomerPage";
             this.Text = "CustomerPage";
             this.Load += new System.EventHandler(this.CustomerPage_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgCustomerInfo)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -215,12 +204,11 @@ namespace Group29_BlitzCafe
         #endregion
 
         private System.Windows.Forms.Button btnAddNew;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dbgCustomerInfo;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnConfirmUpdate;
         private System.Windows.Forms.TextBox txtDate;
         private System.Windows.Forms.Label lblDateJoined;
-        private System.Windows.Forms.CheckBox cbxLoyaltyMem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCellNo;
         private System.Windows.Forms.TextBox txtLName;
--- Group29_BlitzCafe/CustomerPage.cs
+++ Group29_BlitzCafe/CustomerPage.cs
@@ -19,6 +19,11 @@ namespace Group29_BlitzCafe
             InitializeComponent();
         }
         private Default defaultFrm = new Default();
+        private List<Customer> customerList = new List<Customer>();
+
+        private int customerID;
+        private string fName, lName, cellNo;
+        private DateTime dateJoined = new DateTime();
 
         private void CustomerPage_Load(object sender, EventArgs e)
         {
@@ -31,7 +36,26 @@ namespace Group29_BlitzCafe
                     MySqlCommand cmd = new MySqlCommand(query, conn);
                     MySqlDataAdapter dataAdapter = new MySqlDataAdapter(cmd);
                     DataTable dataTable = new DataTable();
-                    dataAdapter.Fill(dataTable)
+                    dataAdapter.Fill(dataTable);
+
+                    dbgCustomerInfo.DataSource = dataTable;
+
+                    //Reading data into List
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        customerID = Convert.ToInt32(row["CustomerID"]);
+                        lName = Convert.ToString(row["Last_Name"]);
+                        fName = Convert.ToString(row["Fisrt_Name"]);
+                        cellNo = Convert.ToString(row["CellNo"]);
+                        dateJoined = Convert.ToDateTime(row["Date_Joined"]);
+
+                        // Create a new Customer object using the data
+                        Customer customerObj = new Customer(customerID, lName, fName, cellNo, dateJoined);
+
+                        // Add the Customer object to the list
+                        customerList.Add(customerObj);
+                    }
+
                 }
                 catch (Exception ex)
                 {
@@ -49,16 +73,31 @@ namespace Group29_BlitzCafe
 
         private void btnAddNew_Click(object sender, EventArgs e)
         {
-            string fName = txtFName.Text, lName = txtLName.Text, cellNo = txtCellNo.Text;
-            bool isLoylMem = cbxLoyaltyMem.Checked;
-            DateTime dateJoined = new DateTime();
-
+            fName = txtFName.Text;
+            lName = txtLName.Text;
+            cellNo = txtCellNo.Text;
             dateJoined = DateTime.Today;
 
             if (cellNo.Length == 10 && !string.IsNullOrWhiteSpace(txtFName.Text) && !string.IsNullOrWhiteSpace(txtLName.Text))
             {
-                //Customer addNew = new Customer(1, lName, fName, cellNo, isLoylMem, dateJoined);
-                MessageBox.Show(lName + " " + fName + " " + cellNo +" " + isLoylMem.ToString() + " "+ dateJoined.ToString());
+                //Customer addNew = new Customer(1, lName, fName, cellNo, dateJoined);
+                MessageBox.Show(lName + " " + fName + " " + cellNo + " "+ dateJoined.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please make sure all info is entered and correct.");
+            }
+        }
+
+        private void btnConfirmUpdate_Click(object sender, EventArgs e)
+        {
+            string fName = txtFName.Text, lName = txtLName.Text, cellNo = txtCellNo.Text;
+            bool isLoylMem = cbxLoyaltyMem.Checked;
+
+            if (cellNo.Length == 10 && !string.IsNullOrWhiteSpace(txtFName.Text) && !string.IsNullOrWhiteSpace(txtLName.Text))
+            {
+                //Customer addNew = new Customer(1, lName, fName, cellNo, dateJoined);
+                MessageBox.Show(lName + " " + fName + " " + cellNo);
             }
             else
             {
