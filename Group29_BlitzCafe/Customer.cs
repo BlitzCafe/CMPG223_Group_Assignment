9bb09f7a6120fdf8176d9d9f87162261b4bd917e
Juane Schoeman <138971307+JuaneSchoeman@users.noreply.github.com>
2024-08-09 11:40:45 +0200

    Added Customer class

--- Group29_BlitzCafe/Customer.cs
+++ Group29_BlitzCafe/Customer.cs
@@ -8,6 +8,85 @@ namespace Group29_BlitzCafe
 {
     class Customer
     {
-        
+        private readonly int customerID;
+        private string lastName, firstName;
+        private string cellNo;
+        private bool isLoyatyMem;
+        private DateTime date_Joined;
+
+        //Constructor
+        public Customer(int customerID, string lastName, string firstName, string cellNo, bool isLoyatyMem, DateTime date_Joined)
+        {
+            this.customerID = customerID;
+            setLastName(lastName);
+            setFirstName(firstName);
+            setCellNo(cellNo);
+            setIsLoyaltyMem(isLoyatyMem);
+            setDateJoined(date_Joined);
+        }
+
+        //Accessors
+
+        public void setLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        public void setFirstName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public void setCellNo(string cellNo)
+        {
+            this.cellNo = cellNo;
+        }
+
+        public void setIsLoyaltyMem(bool isLoyatyMem)
+        {
+            this.isLoyatyMem = isLoyatyMem;
+        }
+
+        public void setDateJoined(DateTime date_Joined)
+        {
+            this.date_Joined = date_Joined;
+        }
+
+        //Mutators
+        public int getCustomerID()
+        {
+            return customerID;
+        }
+
+        public string getLastName()
+        {
+            return lastName;
+        }
+
+        public string getFirstName()
+        {
+            return firstName;
+        }
+
+        public string getCellNo()
+        {
+            return cellNo;
+        }
+
+        public bool getIsLoyaltyMem()
+        {
+            return isLoyatyMem;
+        }
+
+        public DateTime getDateJoined()
+        {
+            return date_Joined;
+        }
+
+        //ToString method
+        public string toString()
+        {
+            return "Customer ID: " + getCustomerID() + "\tFirst Name: " + getFirstName() + "\tLast Name: " + getLastName() + "\tCellphone Number: " + getCellNo() + "\tIs Loyalty member?: " + getIsLoyaltyMem() + "\tDate Joined: " + getDateJoined();
+        }
     }
 }
